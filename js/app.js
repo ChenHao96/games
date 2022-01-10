b05cf07ef83ff680a0f68fdf1017bc5cab61394e
Steven-Work <949727157@qq.com>
2022-01-10 09:47:19 +0800

    update

--- js/app.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    GameWorldManager.setDirection(GameWorldManager.Direction.portraiture)
-    GameWorldManager.setWorldSize(1080, 1920)
-    GameScreenClick.activityClick()
-    GameDrawManager.run()
-})()
